0c38b6cddb1179f2465eb2b7a98c7ed26300b95f
AlexChuzha <chuzhaalexander@gmail.com>
2016-10-26 17:34:40 +1200

    26.10.2016

---
+++ MVCchat/Models/ChatModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace AjaxChat.Models
+{
+    public class ChatModel
+    {
+        public List<ChatUser> Users;  // Все пользователи чата
+
+        public List<ChatMessage> Messages; // все сообщения
+
+        public ChatModel()
+        {
+            Users = new List<ChatUser>();
+            Messages = new List<ChatMessage>();
+
+            Messages.Add(new ChatMessage()
+            {
+                Text = "Чат запущен " + DateTime.Now
+            });
+        }
+    }
+    public class ChatUser
+    {
+        public string Name;
+        public DateTime LoginTime;
+        public DateTime LastPing;
+    }
+
+    public class ChatMessage
+    {
+        // автор сообщения, если null - автор сервер
+        public ChatUser User;
+        // время сообщения
+        public DateTime Date = DateTime.Now;
+        // текст сообщения
+        public string Text = "";
+    }
+}
+
---
+++ MVCchat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCchat.Startup))]
+namespace MVCchat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
